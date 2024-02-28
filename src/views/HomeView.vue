6a64defc48a39784cd98cf701b868120e7c28301
GiuliaCarangelo <tnhkagura@gmail.com>
2024-02-28 22:32:55 +0100

    add intro main text and paragraph

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,24 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <title>Cocktails</title>
     </head>
-    <body>
-        <img class="bg-image" src="../assets/final landing page mockup.png" alt="Background Mojito Image">
-        
+    <body class="d-flex align-content-center">
+        <div class="d-flex">
+            <h1 class="first-main-title text-white pe-4 pb-2 fw-bolder">Cocktail</h1>
+            <h1 class="main-title text-white fw-lighter">Bar</h1>
+        </div>
+        <section>
+            <p class="home-description text-white fw-medium">
+                Benvenuti nel nostro cocktail bar, dove l'eleganza si unisce all'innovazione per offrirvi un'esperienza unica. Con cocktail artigianali e un'atmosfera raffinata, ogni sorso è un viaggio sensoriale. Venite a scoprire il gusto della nostra creatività in ogni bicchiere.
+            </p>
+            <button class="index-btn">
+                <a href="">
+
+                </a>
+            </button>
+        </section>
     </body>
 
     </html>
